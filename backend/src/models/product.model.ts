3427198ed3cd68d74afb6906f062ebcf7997905b
AnuragDahal <freefirepros2020@gmail.com>
2024-10-08 00:38:35 +0545

    Add product model with required fields and default values

---
+++ backend/src/models/product.model.ts
@@ -0,0 +1,95 @@
+import mongoose, { Types } from "mongoose";
+// _id ObjectId pk
+// name string
+// description string
+// price number
+// image string //imagekit or cloudinary url
+// category Enum
+// totalitemsSold number
+// totalStocksLeft number
+// isoutOfStocks true
+// createdAt Date
+// updatedAt Date
+// discount number
+// reviews Array
+// sellerId Objectid['seller']
+
+interface IProduct extends mongoose.Document {
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: Array<string>;
+    category: string;
+    totalQuantity: number;
+    countInStock: number;
+    totalItemsSold: number;
+    isOutOfStock: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    discount: number;
+    reviews: Array<string>;
+    sellerId: Types.ObjectId;
+}
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    imageUrl: {
+        type: Array,
+        required: true,
+        default: [],
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    totalQuantity: {
+        type: Number,
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        default: function (this: any) {
+            return this.totalQuantity - this.totalItemsSold;
+        },
+    },
+    totalItemsSold: {
+        type: Number,
+        default: 0,
+    },
+    discount: {
+        type: Number,
+        default: 0,
+    },
+    isOutOfStock: {
+        type: Boolean,
+        default: function (this: any) {
+            if (this.countInStock === 0) {
+                return true;
+            }
+            return false;
+        },
+    },
+    reviews: {
+        type: Array,
+        default: [],
+    },
+    sellerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Seller",
+        required: true,
+    },
+});
+
+const Product = mongoose.model<IProduct>("Product", ProductSchema);
+export default Product;
