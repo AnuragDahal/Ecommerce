fde1556316351d82c59758e83b2c373ab7019eab
AnuragDahal <freefirepros2020@gmail.com>
2024-10-07 11:16:06 +0545

    Refactor API routes in auth.routes.ts

--- backend/src/apis/auth.routes.ts
+++ backend/src/apis/auth.routes.ts
@@ -10,6 +10,14 @@ const router = Router();
 
 router.post("/signup", handleSignUp);
 router.post("/login", handleLogin);
+
 // router.post("/logout", handleLogout);
+
+router.post("/logout", handleLogout);
+// router.post("/change-password");
+// router.post("/forgot-password" /* handler */);
+// router.post("/reset-password" /* handler */);
+// router.post("/verify-email" /* handler */);
+// router.post("/resend-verification-email" /* handler */);
 
 export default router;
